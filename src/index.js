af4229744081fc19d9864f62f7da15c008ad02a6
jiyun99 <rrwldbs@gmail.com>
2021-09-17 23:31:53 -0400

    update index and test files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
 
+const getHTML = (title, contents) => `<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>${title}</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+${contents}
+</body>
+</html>
+`
 
-function main() {
+async function main() {
     const { argv } = yargs(hideBin(process.argv))
         .option('i', {
             alias: 'input',
@@ -17,6 +30,32 @@ function main() {
         .version('v', '0.0.1')
         .alias('h', 'help')
         .help('h', 'Display help');
+
+    const { input } = argv; // const input = argv.input; 
+    console.log(input);
+
+    let files = [];
+    if (fs.lstatSync(input).isDirectory()) {
+        const filesFromDir = await fs.promises.readdir(input, 'utf-8');
+        files = filesFromDir.map(e => `${input}/${e}`);
+    } else {
+        files.push(input);
+    }
+    console.log(files);
+
+    for (const file of files) {
+
+        const res = await fs.promises.readFile(file, 'utf-8');
+        let resArr = res.split('\n\n');
+        resArr = resArr.map(e => `<p>${e}</p>\n`);
+        // console.log(resArr);
+        const fileNameExt = file.split('/')[file.split('/').length - 1];
+        const filename = fileNameExt.split('.')[0];
+        if (!fs.existsSync('dist')) {
+            fs.mkdirSync('dist');
+        }
+        await fs.promises.writeFile(`dist/${filename}.html`, getHTML(filename, resArr.join("")));
+    }
 
 }
 
